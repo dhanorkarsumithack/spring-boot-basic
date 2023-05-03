8116c58a9c89f7b045ab002970b416c6b75e39ce
dhanorkarsumithack <91867665+dhanorkarsumithack@users.noreply.github.com>
2023-05-03 19:12:25 +0530

    Delete authentication-service directory

--- authentication-service/src/main/java/com/neml/repositories/RegistrationMasterRepo.java
+++
@@ -1,9 +0,0 @@
-package com.neml.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.neml.entities.RegistrationMaster;
-
-public interface RegistrationMasterRepo extends JpaRepository<RegistrationMaster, String> {
-
-}
--- authentication-service/src/main/java/com/neml/service/OTPService.java
+++
@@ -1,9 +0,0 @@
-package com.neml.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public interface OTPService {
-    int generateOTP();
-
-}
